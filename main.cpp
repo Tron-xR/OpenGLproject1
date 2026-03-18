6b2a4047e3b36844e2969b5e91c6ca7f0fb46977
Tron-xR <tron.x.530@gmail.com>
2026-03-18 19:37:03 +0530

    y

--- main.cpp
+++ main.cpp
@@ -20,6 +20,8 @@ float vertices[] = {
 
 int main() {
 	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
